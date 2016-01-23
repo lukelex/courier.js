360ab5ee4b673e6024d7f5621769e2f6f9b1519c
Raphael Faria <raphaelfmf@gmail.com>
2016-01-23 13:32:47 -0200

    Fixes matching of partial string and regexps

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,3 +1,11 @@
-export function stringify( name ) {
-  return name.toString().replace( /(^\/|\/$)/g, "" );
+export function typeOf( obj ) {
+  if (typeof obj === 'string' || obj instanceof String) {
+    return 'string';
+  }
+
+  if (obj instanceof RegExp) {
+    return 'regexp';
+  }
+
+  return typeof obj;
 }
